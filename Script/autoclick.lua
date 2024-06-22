b7e7cc864b1698ff5c9718af1d50083a14dffc99
Mahi <122893983+25802906@users.noreply.github.com>
2024-06-22 13:40:39 +0700

    Update autoclick.lua

--- Script/autoclick.lua
+++ Script/autoclick.lua
@@ -61,7 +61,11 @@ end)
 -- Auto Click
 while (true) do
     Text.Visible = Settings.GUI
-    Text.Position = Vector2.new(Camera.ViewportSize.X - 133, Camera.ViewportSize.Y - 48)
+
+    -- Điều chỉnh vị trí của GUI ở đây
+    local offsetX = 50  -- Giá trị tùy chỉnh theo ý bạn
+    local offsetY = 50  -- Giá trị tùy chỉnh theo ý bạn
+    Text.Position = Vector2.new(Camera.ViewportSize.X - offsetX, Camera.ViewportSize.Y - offsetY)
 
     if (flags.Auto_Clicking) then
         for i = 1, 2 do
